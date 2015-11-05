a8ad35121de966d2d9a9d9d41db335b0eeaf4dfa
Carl-Erik Kopseng <carlerik@gmail.com>
2015-11-05 01:10:48 +0100

    Oppdater LF med ny løsning med hjelpefunksjoner

--- code.js
+++ code.js
@@ -1,26 +1,46 @@
+/**
+ * Create a new function 
+ *
+ * @arg {*} the argument that the new function will return
+ * @returns {Function} a new function
+ */
 function functionCreator(arg){
     return function() {
         return arg;
     }
 }
 
+/**
+ * Get the individual arguments as an array
+ *
+ * @returns [] the arguments converted to an array
+ */
 function argumentsToArray(){
-    var args = [].slice.call(arguments,0);
-    return args;
+    return [].slice.call(arguments);
 }
 
 /**
  * Function that "remembers" previous calls to save computation
+ * Yes, we are talking about caching.
+ *
  * @param {Function} fn a function
  */
 function memoize(fn){
     var cache = {};
 
     return function(){
-        var key = [].join.call(arguments);
+        /* 
+         * Convert the arguments array to  a true array.
+         * As the function is expecting multiple arguments, we need
+         * to apply our list of arguments to the function to be able to utilize it
+         */
+        var args = argumentsToArray.apply(null,arguments);
+        // Convert the argument array to a string - this only works with a true array
+        var key = args.join();
+
         if(cache[key]) return cache[key];
 
-        var res = fn.apply(null, arguments);
+        var res = fn.apply(null, args);
         cache[key] = res;
         return res;
     }
