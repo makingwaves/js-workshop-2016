8fa03b277621da97fe6e7cafb670c89e912f14dd
Carl-Erik Kopseng <carlerik@gmail.com>
2015-11-05 09:34:29 +0100

    Legg til mer dokumentasjon og hjelper :100:
    Det var litt for lett å gjenbruke hjelperfunksjonenen vi
    laget i basic til å forsøke å kopiere 'arguments'-objektet
    pga navnet - i steden for de individuelle elementene.
    
    For å unngå forvirring laget jeg en ekstra hjelper som gjør
    nesten det samme: forskjellen er kun at den faktisk kopierer
    det første argumentet inn i en ny array.
    
    Og at den heter noe fornuftig ...

--- code.js
+++ code.js
@@ -1,4 +1,13 @@
 /**
+ * By having a direct reference to the slice function of the Array 
+ * prototype object we get some speedups
+ *
+ * 1. We don't need to travel up the prototype chain when referencing the function
+ * 2. By directly referencing the prototype we avoid having to create a new object each time
+ */
+var slice = Array.prototype.slice;
+
+/**
  * Create a new function 
  *
  * @arg {*} the argument that the new function will return
@@ -14,9 +23,32 @@ function functionCreator(arg){
  * Get the individual arguments as an array
  *
  * @returns [] the arguments converted to an array
+ * 
+ * @example
+ * argumentsToArray(1,2,3,4) // => [1,2,3,4];
  */
 function argumentsToArray(){
-    return [].slice.call(arguments);
+    return slice.call(arguments);
+}
+
+
+/**
+ * Copy the individual elements of an array-like object into a new array
+ *
+ * @param myArray the array (or array-like object)
+ * @returns a new array
+ * 
+ * @example
+ * var arr1 = [1,2,3,4];
+ * var arr2 = argumentsToArray(arr1)
+ * arr1 !== arr2 // ==> true
+ */
+function copyArrayLikeObject(myArray){
+    // usually one would use something like the 'check-types' library to do this
+    if(!myArray.hasOwnProperty('length') ) {
+        throw new TypeError('We do not know how to handle this object');
+    }
+    return slice.call(myArray);
 }
 
 /**
@@ -34,7 +66,7 @@ function memoize(fn){
          * As the function is expecting multiple arguments, we need
          * to apply our list of arguments to the function to be able to utilize it
          */
-        var args = argumentsToArray.apply(null,arguments);
+        var args = copyArrayLikeObject(arguments);
         // Convert the argument array to a string - this only works with a true array
         var key = args.join();
 
